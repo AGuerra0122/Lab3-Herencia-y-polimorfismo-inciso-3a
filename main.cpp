2a08f9d0ac96112c2aca0ba2fdc819121938b62e
AGuerra0122 <113212825+AGuerra0122@users.noreply.github.com>
2022-10-03 19:03:41 -0600

    Link de github

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
  // Description:     Uso de clases, herencias y Polimorfismo para diseñar un ADC de 32 canales, con resolucion de 8, 10 o 12 bits y un rango de entrada de 0 a 3.3v
  // Authors:         Jesus Adrian Guerra Delgado
  // Updated:         09/2022
+ // LINK a Github: https://github.com/AGuerra0122/Lab3-Herencia-y-polimorfismo-inciso-3a 
  //Nota: Version 3a
 ************************************************************************************************************************/
 //30-09-2022
